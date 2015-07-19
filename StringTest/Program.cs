b7f3e5f56f2d09b421a594a124f6c1b338e5531f
unosbaghaii <unos.bm65@yahoo.com>
2015-07-19 13:21:17 +0430

    StringTest

---
+++ StringTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass c=new MyClass();
+            c.m1();
+
+        }
+
+        class MyClass
+        {
+            
+            public void m1()
+            {
+                var str = "ali";
+                Console.WriteLine(str);
+                m2(ref str);
+                Console.WriteLine(str);
+                Console.ReadLine();
+            }
+
+
+            public void m2(ref string name)
+            {
+                var s = name;
+                name = "changed name";
+
+            }
+        }
+
+    }
+}
