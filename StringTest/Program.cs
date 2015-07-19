8f16bf9826967255d3fb102f8de8886d13ceb974
unosbaghaii <unos.bm65@yahoo.com>
2015-07-19 13:44:47 +0430

    using ref with strings

--- StringTest/Program.cs
+++ StringTest/Program.cs
@@ -10,20 +10,44 @@ namespace StringTest
     {
         static void Main(string[] args)
         {
-            MyClass c=new MyClass();
+            TestStringImmutable c = new TestStringImmutable();
             c.m1();
 
+
+            TestString T = new TestString();
+            T.m1();
         }
 
-        class MyClass
+        class TestStringImmutable
         {
             
             public void m1()
             {
                 var str = "ali";
-                Console.WriteLine(str);
+                Console.WriteLine("TestStringImmutable =  before calling method str is =  " + str);
+                m2(str);
+                Console.WriteLine("TestStringImmutable =  after calling method str is =  " + str);
+                Console.ReadLine();
+            }
+
+
+            public void m2(string name)
+            {
+                var s = name;
+                name = "changed name";
+                Console.WriteLine("inside the method name is =  " + name);
+
+            }
+        }
+        class TestString
+        {
+
+            public void m1()
+            {
+                var str = "ali";
+                Console.WriteLine("TestString  with ref=  before calling method str is =  " + str);
                 m2(ref str);
-                Console.WriteLine(str);
+                Console.WriteLine("TestString with ref =  after calling method str is =  " + str);
                 Console.ReadLine();
             }
 
@@ -32,6 +56,7 @@ namespace StringTest
             {
                 var s = name;
                 name = "changed name";
+                Console.WriteLine("inside the method name is =  " + name);
 
             }
         }
